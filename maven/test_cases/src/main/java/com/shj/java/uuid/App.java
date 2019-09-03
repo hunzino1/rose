18c17337e06832fb8341e5415be9f17974c54a78
sunhaojie <sunhaojie02@meituan.com>
2019-09-03 11:20:53 +0800

    null

---
+++ maven/test_cases/src/main/java/com/shj/java/uuid/App.java
@@ -0,0 +1,15 @@
+package com.shj.java.uuid;
+
+import java.util.UUID;
+
+/**
+ * @description:
+ * @author: sunhaojie
+ * @create: 2019/08/29 17:36
+ */
+public class App {
+    public static void main(String[] args) {
+        UUID.randomUUID();
+        System.out.println(UUID.randomUUID());
+    }
+}
